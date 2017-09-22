6a546502f8d6a47fca1ce74bfc0d183af2d1f576
Sara <saraahmad1114@yahoo.com>
2017-09-22 00:46:25 -0400

    added StarWarsPeopleAPIClient

---
+++ StarWarsAPI/StarWarsAPI/StarWarsPeopleAPIClient.swift
@@ -0,0 +1,19 @@
+//
+//  StarWarsPeopleAPIClient.swift
+//  StarWarsAPI
+//
+//  Created by Flatiron School on 9/22/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class StarWarsPeopleAPIClient{
+
+
+
+
+
+
+
+}
