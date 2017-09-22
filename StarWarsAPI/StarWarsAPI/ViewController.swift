dee739b8791049540a455ad13158bb81a534b6c9
Sara <saraahmad1114@yahoo.com>
2017-09-21 21:10:07 -0400

    added outlets for your button

--- StarWarsAPI/StarWarsAPI/ViewController.swift
+++ StarWarsAPI/StarWarsAPI/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var peopleButton: UIButton!
+    
+    @IBOutlet weak var planetsButton: UIButton!
+    
+    @IBOutlet weak var starshipsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
