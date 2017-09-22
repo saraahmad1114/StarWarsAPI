c159fc564b90260b4b89f7429a8ff4c88f7c4960
Sara <saraahmad1114@yahoo.com>
2017-09-22 02:17:10 -0400

    added StarWarsStarshipsAPIClient

---
+++ StarWarsAPI/StarWarsAPI/StarWarsStarshipsAPIClient.swift
@@ -0,0 +1,26 @@
+//
+//  StarWarsStarshipsAPIClient.swift
+//  StarWarsAPI
+//
+//  Created by Flatiron School on 9/22/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class StarWarsStarshipsAPIClient{
+    
+    class func getStarWarsStarshipsInformation (completion: @escaping(Array<Any>)-> ()){
+    
+        
+        
+    
+        
+    
+    }
+
+
+
+
+
+}
