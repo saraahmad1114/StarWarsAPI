2bd2dd2dbe9d125c4884bbfbc401e122c07dabe7
Sara <saraahmad1114@yahoo.com>
2017-10-19 23:19:23 -0400

    added peopletableView controller

---
+++ StarWarsAPI/StarWarsStarship.swift
@@ -0,0 +1,16 @@
+//
+//  StarWarsStarship.swift
+//  StarWarsAPI
+//
+//  Created by Flatiron School on 9/22/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class StarWarsStarship {
+
+    
+
+
+}
