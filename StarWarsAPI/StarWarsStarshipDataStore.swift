84bd5e8b654ce4cbfe9f80540639b09f5d7f6b81
Sara <saraahmad1114@yahoo.com>
2017-10-21 23:51:49 -0400

    StarWarsStarshipDataStore

---
+++ StarWarsAPI/StarWarsStarshipDataStore.swift
@@ -0,0 +1,19 @@
+//
+//  StarWarsStarshipDataStore.swift
+//  StarWarsAPI
+//
+//  Created by Flatiron School on 10/21/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class StarWarsStarshipDataStore{
+    
+    static let sharedInstance = StarWarsStarshipDataStore()
+    private init() {}
+    
+    
+    
+    
+}
