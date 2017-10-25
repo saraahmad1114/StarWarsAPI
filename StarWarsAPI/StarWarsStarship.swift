ee7b9540fcf88926cb8d58b0768ba9e72828e335
Sara <saraahmad1114@yahoo.com>
2017-10-24 21:37:31 -0400

    StarWarsStarship

--- StarWarsAPI/StarWarsStarship.swift
+++ StarWarsAPI/StarWarsStarship.swift
@@ -2,15 +2,34 @@
 //  StarWarsStarship.swift
 //  StarWarsAPI
 //
-//  Created by Flatiron School on 9/22/17.
+//  Created by Flatiron School on 10/23/17.
 //  Copyright © 2017 Flatiron School. All rights reserved.
 //
 
 import Foundation
 
-class StarWarsStarship {
-
+class StarWarsStarship{
     
-
-
+    var name: String?
+    var model: String?
+    var manufacturer: String?
+    var costInCredits: String?
+    var length: String?
+    var maxAtmosphericSpeed: String?
+    var crew: String?
+    var cargoCapacity: String?
+    var starshipClass: String?
+    
+    init(name: String, model: String, manufacturer: String, costInCredits: String, length: String, maxAtmosphericSpeed: String, crew: String, cargoCapacity: String, starshipClass: String) {
+        self.name = name
+        self.model = model
+        self.manufacturer = manufacturer
+        self.costInCredits = costInCredits
+        self.length = length
+        self.maxAtmosphericSpeed = maxAtmosphericSpeed
+        self.crew = crew
+        self.cargoCapacity = cargoCapacity
+        self.starshipClass = starshipClass
+    }
+    
 }
