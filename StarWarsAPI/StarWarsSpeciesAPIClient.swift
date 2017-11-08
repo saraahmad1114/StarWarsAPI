95e14ec5422e94acf5ccf7c78a162017d030d059
saraahmad1114 <asara1114@gmail.com>
2017-11-08 15:11:11 -0500

    StarWarsSpeciesAPIClient

---
+++ StarWarsAPI/StarWarsSpeciesAPIClient.swift
@@ -0,0 +1,19 @@
+//
+//  StarWarsSpeciesAPIClient.swift
+//  StarWarsAPI
+//
+//  Created by Sara Ahmad on 11/8/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class StarWarsSpeciesAPIClient {
+    
+    
+    
+    
+    
+    
+    
+}
