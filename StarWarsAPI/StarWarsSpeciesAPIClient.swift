1017013af0f3fdfecbe020cd855181db8995df44
saraahmad1114 <asara1114@gmail.com>
2017-11-08 15:23:26 -0500

    completed  StarWarsSpeciesAPIClient

--- StarWarsAPI/StarWarsSpeciesAPIClient.swift
+++ StarWarsAPI/StarWarsSpeciesAPIClient.swift
@@ -10,10 +10,38 @@ import Foundation
 
 class StarWarsSpeciesAPIClient {
     
+    class func getStarWarsSpeciesInformation (completion: @escaping(Array<Any>)-> ()){
+        
+        var jsonArray = Array<Any>()
+        
+        let url = "https://swapi.co/api/species"
+        
+        let convertedUrl = URL(string: url)
+        
+        guard let unwrappedConvertedUrl = convertedUrl else {print("unwrappedConvertedUrl did not unwrap"); return}
+        
+        let request = URLRequest(url: unwrappedConvertedUrl)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let unwrappedData = data else{print("unwrappedData did not unwrap"); return}
+            
+            let jsonDictionary = try? JSONSerialization.jsonObject(with: unwrappedData, options: []) as! [String: Any]
+            
+            guard let unwrappedJsonDictionary = jsonDictionary else {print("unwrappedJsonDictionary did not unwrap"); return}
+            
+            let resultsArray = unwrappedJsonDictionary["results"] as? Array<Any>
+            
+            guard let unwrappedResultsArray = resultsArray else {print("unwrappedResultsArray did not unwrap"); return}
+            
+            jsonArray = unwrappedResultsArray
+            
+            completion(jsonArray)
+            
+        }
+        task.resume()
+        
+    }
     
-    
-    
-    
-    
-    
+
 }
