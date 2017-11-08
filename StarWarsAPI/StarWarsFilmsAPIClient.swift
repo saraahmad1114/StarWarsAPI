150cbdcd8007766014ca259d99a607d5261af98a
saraahmad1114 <asara1114@gmail.com>
2017-11-08 10:46:47 -0500

    added the StarWarsFilmsAPIClient class

---
+++ StarWarsAPI/StarWarsFilmsAPIClient.swift
@@ -0,0 +1,20 @@
+//
+//  StarWarsFilmsAPIClient.swift
+//  StarWarsAPI
+//
+//  Created by Sara Ahmad on 11/8/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class StarWarsFilmsAPIClient{
+    
+    
+    
+    
+    
+    
+    
+    
+}
