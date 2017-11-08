49294385648ee2846be29c171d0a120c45f6d489
saraahmad1114 <asara1114@gmail.com>
2017-11-08 15:00:13 -0500

    StarWarsFilmsAPIClient

--- StarWarsAPI/StarWarsFilmsAPIClient.swift
+++ StarWarsAPI/StarWarsFilmsAPIClient.swift
@@ -10,11 +10,32 @@ import Foundation
 
 class StarWarsFilmsAPIClient{
     
-    
-    
-    
-    
-    
-    
+    class func getStarWarsFilmsInformation (completion:@escaping([String: Any]) -> ()){
+        
+        var filmsJSon = [String: Any]()
+        
+        let filmsUrl = "https://swapi.co/api/films"
+        
+        let convertedUrl = URL(string: filmsUrl)
+        
+        guard let unwrappedConvertedUrl = convertedUrl else {print("convertedUrl did not unwrap"); return}
+        
+        let request = URLRequest(url: unwrappedConvertedUrl)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            guard let unwrappedData = data else {print("data did not unwrap"); return}
+            
+            let jsonDictionary = try? JSONSerialization.jsonObject(with: unwrappedData, options: []) as! [String: Any]
+            
+            guard let uwnrappedJsonDictionary = jsonDictionary else {print("jsonDictionary did not unwrap"); return}
+            
+            filmsJSon =  uwnrappedJsonDictionary
+            
+            completion(filmsJSon)
+        }
+        task.resume()
+    }
+
     
 }
