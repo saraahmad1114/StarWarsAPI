e19554417860482fde0f9411cd4bef284a662e3a
saraahmad1114 <asara1114@gmail.com>
2017-11-11 23:11:22 -0500

    finished StarWarsStarship

--- StarWarsAPI/StarWarsStarship.swift
+++ StarWarsAPI/StarWarsStarship.swift
@@ -21,15 +21,29 @@ class StarWarsStarship{
     var starshipClass: String?
     
     init(jsonDictionary: [String: Any]) {
-        self.name = jsonDictionary["name"] as? String
-        self.model = jsonDictionary["model"] as? String
-        self.manufacturer = jsonDictionary["manufacturer"] as? String
-        self.costInCredits = jsonDictionary["cost_in_credits"] as? String
-        self.length = jsonDictionary["length"] as? String
-        self.maxAtmosphericSpeed = jsonDictionary["max_atmosphering_speed"] as? String
-        self.crew = jsonDictionary["crew"] as? String
-        self.cargoCapacity = jsonDictionary["cargo_capacity"] as? String
-        self.starshipClass = jsonDictionary["starship_class"] as? String
-    }
+        
+        guard
+            
+        let name = jsonDictionary["name"] as? String,
+        let model = jsonDictionary["model"] as? String,
+        let manufacturer = jsonDictionary["manufacturer"] as? String,
+        let costInCredits = jsonDictionary["cost_in_credits"] as? String,
+        let length = jsonDictionary["length"] as? String,
+        let maxAtmosphericSpeed = jsonDictionary["max_atmosphering_speed"] as? String,
+        let crew = jsonDictionary["crew"] as? String,
+        let cargoCapacity = jsonDictionary["cargo_capacity"] as? String,
+        let starShipClass = jsonDictionary["starship_class"] as? String
+        
+            else {print("did not unwrap"); return}
+        
+        self.name = name
+        self.model = model
+        self.manufacturer = manufacturer
+        self.costInCredits = costInCredits
+        self.length = length
+        self.maxAtmosphericSpeed = maxAtmosphericSpeed
+        self.crew = crew
+        self.cargoCapacity = cargoCapacity
+        self.starshipClass = starshipClass
     
 }
