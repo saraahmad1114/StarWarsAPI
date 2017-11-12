40d741bc47a51f982b15289a1a471aa00f357549
saraahmad1114 <asara1114@gmail.com>
2017-11-11 23:21:01 -0500

    finished StarWarsFilms

--- StarWarsAPI/StarWarsFilms.swift
+++ StarWarsAPI/StarWarsFilms.swift
@@ -10,7 +10,31 @@ import Foundation
 
 class StarWarsFilms{
     
+    var title: String?
+    var episodeId: Int?
+    var opening: String?
+    var director: String?
+    var producer: String?
+    var releaseDate: String?
     
-    
+    init(jsonDictionary: [String: Any]) {
+        
+        guard
+            let title = jsonDictionary["title"] as? String,
+            let episodeId = jsonDictionary["episode_id"] as? Int,
+            let opening = jsonDictionary["opening_crawl"] as? String,
+            let director = jsonDictionary["director"] as? String,
+            let producer = jsonDictionary["producer"] as? String,
+            let releaseDate = jsonDictionary["release_date"] as? String
+        
+            else{print("did not unwrap"); return}
+        
+        self.title = title
+        self.episodeId = episodeId
+        self.opening = opening
+        self.director = director
+        self.producer = producer
+        self.releaseDate = releaseDate
+    }
     
 }
