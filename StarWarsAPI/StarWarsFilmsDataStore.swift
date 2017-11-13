107481aaf9f2c40b46aebb994e9fc9d5e43ae7d9
saraahmad1114 <asara1114@gmail.com>
2017-11-13 15:10:26 -0500

    created StarWarsFilmsDataStore

---
+++ StarWarsAPI/StarWarsFilmsDataStore.swift
@@ -0,0 +1,18 @@
+//
+//  StarWarsFilmsDataStore.swift
+//  StarWarsAPI
+//
+//  Created by Sara Ahmad on 11/13/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class StarWarsFilmsDataStore {
+    
+    
+    
+    
+    
+    
+}
