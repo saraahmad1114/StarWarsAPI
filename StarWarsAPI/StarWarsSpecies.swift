fc5ee3379663b9ba90c4c89f80bd92e5a0e0e8d9
saraahmad1114 <asara1114@gmail.com>
2017-11-12 22:14:45 -0500

    created the  StarWarsVehicleDataStore

--- StarWarsAPI/StarWarsSpecies.swift
+++ StarWarsAPI/StarWarsSpecies.swift
@@ -1,4 +1,4 @@
-//
+
 //  StarWarsSpecies.swift
 //  StarWarsAPI
 //
---
+++ StarWarsAPI/StarWarsVehicleDataStore.swift
@@ -0,0 +1,20 @@
+//
+//  StarWarsVehicleDataStore.swift
+//  StarWarsAPI
+//
+//  Created by Sara Ahmad on 11/12/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class StarWarsVehicleDataStore{
+    
+    
+    
+    
+    
+    
+    
+    
+}
