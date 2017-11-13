563e2fa7397534dbb9705838d2a2ec33e5b3808e
saraahmad1114 <asara1114@gmail.com>
2017-11-13 15:16:37 -0500

    completed  StarWarsFilmsDataStore

--- StarWarsAPI/StarWarsFilmsDataStore.swift
+++ StarWarsAPI/StarWarsFilmsDataStore.swift
@@ -10,9 +10,38 @@ import Foundation
 
 class StarWarsFilmsDataStore {
     
+    static let sharedInstance = StarWarsFilmsDataStore()
+    private init() {}
     
+    var starWarsFilmsArray = [StarWarsFilms]()
+    var page = 1
     
-    
-    
-    
+    func getStarWarsFilms (page: Int, completion:@escaping ([StarWarsFilms]) -> ()){
+        
+        StarWarsFilmsAPIClient.getStarWarsFilmsInformation { (fiilmsArray) in
+            
+            for singleDictionary in fiilmsArray{
+                
+                guard let unwrappedSingleDictionary = singleDictionary as? [String: Any] else{print("singleDictionary did not unwrap"); return}
+                
+                var starWarsFilmObject = StarWarsFilms.init(jsonDictionary: unwrappedSingleDictionary)
+                
+                print("**************************************")
+                print(starWarsFilmObject.title)
+                print(starWarsFilmObject.episodeId)
+                print(starWarsFilmObject.opening)
+                print(starWarsFilmObject.director)
+                print(starWarsFilmObject.producer)
+                print(starWarsFilmObject.releaseDate)
+                print("**************************************")
+                
+                self.starWarsFilmsArray.append(starWarsFilmObject)
+                
+                print(self.starWarsFilmsArray.count)
+            }
+            completion(self.starWarsFilmsArray)
+        }
+        
+    }
+  
 }
