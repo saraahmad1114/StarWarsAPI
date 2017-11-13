8e96fc444c009920e3f877babc061530810d223e
saraahmad1114 <asara1114@gmail.com>
2017-11-12 22:23:43 -0500

    completed StarWarsVehicleDataStore

--- StarWarsAPI/StarWarsVehicleDataStore.swift
+++ StarWarsAPI/StarWarsVehicleDataStore.swift
@@ -10,11 +10,39 @@ import Foundation
 
 class StarWarsVehicleDataStore{
     
+    static let sharedInstance = StarWarsVehicleDataStore()
+    private init() {}
+    var starWarsVehicleArray = [StarWarsVehicles]()
     
-    
-    
-    
-    
-    
+    func getStarWarsVehicles(page: Int, completion:@escaping ([StarWarsVehicles]) -> ()){
+        
+        StarWarsVehicleAPIClient.getStarWarsVehicleInformation(page: page) { (jsonArray) in
+            
+            for singleDictionary in jsonArray{
+                
+                guard let unwrappedSingleDictionary = singleDictionary as? [String: Any] else{print("singleDictionary did not unwrap"); return}
+                
+                var starWarsVehicleObject = StarWarsVehicles.init(jsonDictionary: unwrappedSingleDictionary)
+                
+                self.starWarsVehicleArray.append(starWarsVehicleObject)
+                
+                print("*********************************")
+                print(starWarsVehicleObject.name)
+                print(starWarsVehicleObject.model)
+                print(starWarsVehicleObject.manufacturer)
+                print(starWarsVehicleObject.costInCredits)
+                print(starWarsVehicleObject.maxAtmosphericSpeed)
+                print(starWarsVehicleObject.crewCapacity)
+                print(starWarsVehicleObject.vehicleClass)
+                print("*********************************")
+                
+                print(self.starWarsVehicleArray.count)
+                
+            }
+            completion(self.starWarsVehicleArray)
+        }
+        
+        
+    }
     
 }
