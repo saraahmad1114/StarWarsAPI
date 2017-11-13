359d812e327271da0b85296b6debc92f2eaebedc
saraahmad1114 <asara1114@gmail.com>
2017-11-12 22:08:38 -0500

    Completed StarWarsSpecies

--- StarWarsAPI/StarWarsSpecies.swift
+++ StarWarsAPI/StarWarsSpecies.swift
@@ -10,4 +10,38 @@ import Foundation
 
 class StarWarsSpecies{
     
+    var name: String?
+    var classification: String?
+    var designation: String?
+    var averageHeight: String?
+    var skinColor: String?
+    var hairColor: String?
+    var eyeColor: String?
+    var lifespan: String?
+    var language: String?
+    
+    init(jsonDictionary: [String: Any]) {
+        guard
+            let name = jsonDictionary["name"] as? String,
+            let classification = jsonDictionary["classification"] as? String,
+            let designation = jsonDictionary["designation"] as? String,
+            let averageHeight = jsonDictionary["average_height"] as? String,
+            let skinColor = jsonDictionary["skin_colors"] as? String,
+            let hairColor = jsonDictionary["hair_colors"] as? String,
+            let eyeColor = jsonDictionary["eye_colors"] as? String,
+            let lifespan = jsonDictionary["average_lifespan"] as? String,
+            let language = jsonDictionary["language"] as? String
+        
+            else{print("did not unwrap"); return
+    }
+        self.name = name
+        self.classification = classification
+        self.designation = designation
+        self.averageHeight = averageHeight
+        self.skinColor = skinColor
+        self.hairColor = hairColor
+        self.eyeColor = eyeColor
+        self.lifespan = lifespan
+        self.language = language
+    }
 }
