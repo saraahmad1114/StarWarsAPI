fc2786158584c8f1fb6aff002760dcdd6f9cca78
saraahmad1114 <asara1114@gmail.com>
2017-11-21 15:28:28 -0500

    changes

--- StarWarsAPI/StarWarsAPI/ViewController.swift
+++ StarWarsAPI/StarWarsAPI/ViewController.swift
@@ -21,22 +21,22 @@ class ViewController: UIViewController {
     
         self.view.backgroundColor = UIColor.black
 
-        self.peopleButton.layer.shadowColor = UIColor.black.cgColor
-        self.peopleButton.layer.shadowOffset = CGSize(width: 1000, height: 1000)
-        self.peopleButton.layer.masksToBounds = false
-        self.peopleButton.layer.shadowRadius = 5
-        
-        self.peopleButton.layer.shadowOpacity = 0.5
-        self.peopleButton.layer.cornerRadius = self.peopleButton.frame.width / 2
-        self.peopleButton.backgroundColor = .white
-
-
-
-        //starshipsButton
-//        self.starshipsButton.backgroundColor = .yellow
-//        self.starshipsButton.layer.cornerRadius = 8
-//        self.starshipsButton.layer.borderWidth = 1
-//        self.starshipsButton.layer.borderColor = UIColor.white.cgColor
+//        self.peopleButton.layer.shadowColor = UIColor.black.cgColor
+//        self.peopleButton.layer.shadowOffset = CGSize(width: 1000, height: 1000)
+//        self.peopleButton.layer.masksToBounds = false
+//        self.peopleButton.layer.shadowRadius = 5
+//
+//        self.peopleButton.layer.shadowOpacity = 0.5
+//        self.peopleButton.layer.cornerRadius = self.peopleButton.frame.width / 2
+//        self.peopleButton.backgroundColor = .white
+//
+//
+//
+//        //starshipsButton
+////        self.starshipsButton.backgroundColor = .yellow
+////        self.starshipsButton.layer.cornerRadius = 8
+////        self.starshipsButton.layer.borderWidth = 1
+////        self.starshipsButton.layer.borderColor = UIColor.white.cgColor
         
        
     }
