af277be0a4f956f007bb1de696560078e78a8554
saraahmad1114 <asara1114@gmail.com>
2017-12-16 22:48:37 -0500

    wiring up Planets button

--- StarWarsAPI/StarWarsPlanetsTableViewController.swift
+++ StarWarsAPI/StarWarsPlanetsTableViewController.swift
@@ -40,50 +40,22 @@ class StarWarsPlanetsTableViewController: UITableViewController {
     }
 
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "planetCell", for: indexPath)
-        cell.textLabel?.textColor = UIColor.white
-        cell.backgroundColor = UIColor.black
-        cell.textLabel?.text = self.store.starWarsPlanetsArray[indexPath.row].name
-        return cell
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let lastElement = self.store.starWarsPlanetsArray.count-1
+        if indexPath.row == lastElement{
+            loadMoreData()
+        }
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func loadMoreData(){
+        self.store.page = self.store.page + 1
+        self.store.getStarWarsPlanetsInformation(page: self.store.page) { (newArray) in
+            OperationQueue.main.addOperation({
+                self.tableView.reloadData()
+            })
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+    
 
     /*
     // MARK: - Navigation
