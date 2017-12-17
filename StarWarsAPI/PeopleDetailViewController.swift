743588b204030f2165e5d3f012883d207eda177d
saraahmad1114 <asara1114@gmail.com>
2017-12-16 19:27:39 -0500

    clearning

--- StarWarsAPI/PeopleDetailViewController.swift
+++ StarWarsAPI/PeopleDetailViewController.swift
@@ -54,14 +54,4 @@ class PeopleDetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
