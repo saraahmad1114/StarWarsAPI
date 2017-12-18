e0734264a9488758593256d74d61fd4edab2f989
saraahmad1114 <asara1114@gmail.com>
2017-12-18 14:47:02 -0500

    clean pu

--- StarWarsAPI/SpeciesTableViewController.swift
+++ StarWarsAPI/SpeciesTableViewController.swift
@@ -24,18 +24,13 @@ class SpeciesTableViewController: UITableViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return self.store.starWarsSpeciesArray.count
     }
 
@@ -63,16 +58,12 @@ class SpeciesTableViewController: UITableViewController {
             })
         }
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
