7a23eb54cea934b0a31382389dea71ff5f3e30b8
saraahmad1114 <asara1114@gmail.com>
2017-12-20 14:21:54 -0500

    added viewcontroller

--- StarWarsAPI/VehiclesTableViewController.swift
+++ StarWarsAPI/VehiclesTableViewController.swift
@@ -60,11 +60,19 @@ class VehiclesTableViewController: UITableViewController {
             }
         }
     
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "starshipSegue"{
+//            if let destinationVC = segue.destination as? StarshipDetailViewController{
+//                let neededIndexPath = self.tableView.indexPathForSelectedRow!
+//                destinationVC.starShipObject = self.store.starWarsStarshipArray[neededIndexPath.row]
+//            }
+//        }
+//    }
+    
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == 
     }
 
 
