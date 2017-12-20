000336faa7948dd4c313e73f6ac952296b175167
saraahmad1114 <asara1114@gmail.com>
2017-12-20 14:22:25 -0500

    added cocoatouch class

---
+++ StarWarsAPI/VehicleDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VehicleDetailViewController.swift
+//  StarWarsAPI
+//
+//  Created by Sara Ahmad on 12/20/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class VehicleDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
