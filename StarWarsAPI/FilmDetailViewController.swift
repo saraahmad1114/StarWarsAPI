0f23ea04666a5d981ed6f4da52cd6d27d7f6446c
saraahmad1114 <asara1114@gmail.com>
2017-12-22 14:41:53 -0500

    added labels

--- StarWarsAPI/FilmDetailViewController.swift
+++ StarWarsAPI/FilmDetailViewController.swift
@@ -12,15 +12,43 @@ class FilmDetailViewController: UIViewController {
 
     var filmObj: StarWarsFilms? 
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var episodeLabel: UILabel!
+    @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var producerLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var openingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let filmObject = self.filmObj else {print("did not unwrap"); return}
+        
+        guard
+            let title = filmObject.title,
+            let director = filmObject.director,
+            let episodeId = filmObject.episodeId,
+            let producer = filmObject.producer,
+            let opening = filmObject.opening,
+            let releaseDate = filmObject.releaseDate
+            else {print("did not unwrap"); return}
+        
+        //self.view.backgroundColor = UIColor.black
+        
+        self.titleLabel.text = title
+        self.directorLabel.text = director
+        self.episodeLabel.text = String(episodeId)
+        self.producerLabel.text = producer
+        self.releaseDateLabel.text = releaseDate
+        self.openingLabel.text = opening
+        
+        
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
