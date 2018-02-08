030ff6423ddd38177107d7f2da7f780b303c4d08
saraahmad1114 <asara1114@gmail.com>
2018-02-07 21:01:54 -0500

    added labels

--- StarWarsAPI/VehicleDetailViewController.swift
+++ StarWarsAPI/VehicleDetailViewController.swift
@@ -9,7 +9,23 @@ import UIKit
 
 class VehicleDetailViewController: UIViewController {
     
-    var vehicleObj: StarWarsVehicles? 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var manufacturerLabel: UILabel!
+    @IBOutlet weak var costInCreditsLabel: UILabel!
+    @IBOutlet weak var maxAtmosphericSpeedLabel: UILabel!
+    @IBOutlet weak var crewCapacityLabel: UILabel!
+    @IBOutlet weak var vehicleClassLabel: UILabel!
+    
+    @IBOutlet weak var updateName: UILabel!
+    @IBOutlet weak var updateModel: UILabel!
+    @IBOutlet weak var updateManufacturer: UILabel!
+    @IBOutlet weak var updateCost: UILabel!
+    @IBOutlet weak var updateSpeed: UILabel!
+    @IBOutlet weak var updateCrew: UILabel!
+    @IBOutlet weak var updateClass: UILabel!
+    
+    var vehicleObj: StarWarsVehicles?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +35,5 @@ class VehicleDetailViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-
- 
 
 }
